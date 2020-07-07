98b71837322eabfd7cd815a4753acd4007818caa
plasticuproject <19690649+plasticuproject@users.noreply.github.com>
2020-07-07 01:02:36 -0400

    Made the AI harder, it can move vertically now. It can also actually get you when it gets behind you now.

--- get_away.py
+++ get_away.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from pathfinding.core.diagonal_movement import DiagonalMovement
 from pathfinding.core.grid import Grid
 from pathfinding.finder.a_star import AStarFinder
 from random import randint
@@ -12,24 +13,36 @@ levels = [MapOne, MapTwo, MapThree, MapFour, MapFive]
 
 
 # processes pathfinding, draws game frame
-def frame(level, start, end, win):
+def frame(level, new, end, win):
 
     # set up map with nodes
     matrix = level
     grid = Grid(matrix=matrix)
-    start = grid.node(start[0], start[1])
+    start = grid.node(new[0], new[1])
     end = grid.node(end[0], end[1])
     win = grid.node(win[0], win[1])
 
     # pathfinding for NPC AI
-    finder = AStarFinder()
+    #finder = AStarFinder()
+    finder = AStarFinder(diagonal_movement=DiagonalMovement.always)
     path, runs = finder.find_path(start, end, grid)
 
     # print frame
     print(grid.grid_str(path=path, start=start, end=end, win=win))
 
     # return new position of NPC
-    return path[1]
+    print(path[1])
+    print(new)
+    if path[1][0] == new[0] and path[1][1] == new[1] - 1:
+        return (new[0], new[1] - 2)
+    elif path[1][1] == new[1] and path[1][0] == new[0] - 1:
+        return (new[0] - 2, new[1])
+    elif path[1][0] == new[0] and path[1][1] == new[1] + 1:
+        return (new[0], new[1] + 2)
+    elif path[1][1] == new[1] and path[1][0] == new[0] + 1:
+        return (new[0] + 2, new[1])
+    else:
+        return path[1]
 
 
 def controls(direction):
@@ -100,15 +113,14 @@ def main_loop(levels):
 
 if __name__ == '__main__':
 
-    # print welcome screen
+    # run main game loop
     print('\nA game. You are the $. Girlfriend is the &. Run to the @.')
     print('Use wads to move (press return after choice), press q to quit.')
     print('Dotted line is the path your girlfriend plans to take to get you.')
     print('Get to the exit (@) before she hits you with a shoe.')
     print('Press return to start the game.')
     input()
-    
-    # run main game loop    
+
     while True:
         try:
             main_loop(levels)
