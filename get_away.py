2fc27a90e260251601854f694d5eab4e7e397d40
plasticuproject <19690649+plasticuproject@users.noreply.github.com>
2020-07-07 23:05:12 -0400

    removed import statement

--- get_away.py
+++ get_away.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-from pathfinding.core.diagonal_movement import DiagonalMovement
 from pathfinding.core.grid import Grid
 from pathfinding.finder.a_star import AStarFinder
 from random import randint
